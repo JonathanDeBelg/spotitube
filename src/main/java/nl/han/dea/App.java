de6e82cda96d3e9b2aa10472903a307134f40411
JonathanDeBelg <j.dionant@gmail.com>
2019-03-11 11:33:18 +0100

    Add classes user & login

--- src/main/java/nl/han/dea/App.java
+++ src/main/java/nl/han/dea/App.java
@@ -6,8 +6,7 @@ package nl.han.dea;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
