44eb242a7a939d2807458dbeb37ef0492abdebce
JonathanDeBelg <j.dionant@gmail.com>
2019-03-11 11:36:32 +0100

    Basic imput user class

--- src/main/java/nl/han/dea/User.java
+++ src/main/java/nl/han/dea/User.java
@@ -1,4 +1,40 @@
 package nl.han.dea;
 
 public class User {
+    private String name;
+    private String token;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String token, String password) {
+        this.name = name;
+        this.token = token;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
